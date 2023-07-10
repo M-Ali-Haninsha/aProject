390c8ac6fc06a533a193cc2ede8536e702331e4f
M-Ali-Haninsha <alianinsha@gmail.com>
2023-07-10 12:35:39 +0530

    pagesDesign

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserLoginComponent } from './components/user-login/user-login.component';
+import { UserSignupComponent } from './components/user-signup/user-signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'userLogin', component: UserLoginComponent},
+  {path:'userSignup', component: UserSignupComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserSignupComponent } from './components/user-signup/user-signup.component';
+import { LoginComponent } from './components/login/login.component';
+import { UserLoginComponent } from './components/user-login/user-login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserSignupComponent,
+    LoginComponent,
+    UserLoginComponent
   ],
   imports: [
     BrowserModule,
